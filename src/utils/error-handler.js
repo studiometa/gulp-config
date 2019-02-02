733cc627044702bcdfa035c9d540d8c1ab794566
Titouan Mathis <titouan@studiometa.fr>
2019-02-02 13:17:30 +0100

    Format the gulp-uglify errors more nicely
    By default, the source of the error is not shown in the error message,
    we add all the information for better debugging.

--- src/utils/error-handler.js
+++ src/utils/error-handler.js
@@ -4,18 +4,52 @@ import notifier from 'node-notifier';
 const prettyError = new PrettyError();
 
 /**
+ * Format a GulpUglifyError to display the origin of the error
+ * @param  {GulpUglifyError} error A gulp-uglify error object
+ * @return {GulpUglifyError}       A formatted gulp-uglify error
+ */
+const formatUglifyError = (error) => {
+  const { message, line } = error.cause;
+
+  const formatted = `
+${error.fileName}
+Error: ${message}
+       on line ${line} of ${error.fileName}
+`;
+
+  error.originalMessage = error.message;
+  error.message = formatted;
+
+  return error;
+};
+
+
+/**
+ * Format a given error with the correct formatter
+ *
+ * @param  {Error} error The error to format
+ * @return {Error}       The formatted error
+ */
+const formatError = (error) => {
+  return 'cause' in error
+    ? formatUglifyError(error)
+    : error;
+};
+
+
+/**
  * Custom error handler
  *
  * @param  {Error} error The error to display
  * @return {void}
  */
 export default function errorHandler(error) {
+  const formatted = formatError(error);
+  prettyError.render(formatted, true);
   notifier.notify({
     title: 'Error',
-    message: error.message,
+    message: formatted.message,
   });
-
-  prettyError.render(error, true);
 
   return this.emit('end');
 };
